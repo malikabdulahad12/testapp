3fc17e4d6b4c1c6549e93f2acc3549224f2bf690
malikabdulahad12 <50943689+malikabdulahad12@users.noreply.github.com>
2019-05-23 12:36:14 +0500

    Update caredit.blade.php

--- resources/views/caredit.blade.php
+++ resources/views/caredit.blade.php
@@ -3,7 +3,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Laravel MongoDB CRUD Tutorial With Example</title>
+    <title>Laravel 5 MongoDB CRUD Tutorial With Example</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">  
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
